3b3174a9c7a1f1b462b034c2c2775d5bd8f9994d
Ankit <ankit.95sharma@gmail.com>
2022-05-11 21:50:08 +0530

    made task component and declared state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import Header from "./Header";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Interview schedule",
+      day: "May 12th at 04:00pm",
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: "Go to Sanket Wedding",
+      day: "Jun 01st at 09:00am",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Make matrimony profile",
+      day: "May 18th at 04:00pm",
+      reminder: true,
+    },
+  ]);
   return (
-    <div className="App">
-      <Header title="Ankit Sharma" />
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
